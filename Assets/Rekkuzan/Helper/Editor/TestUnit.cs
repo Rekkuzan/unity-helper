9a6a791d7775c9d7b3fd32062156fb38510e191d
Rekkuzan <pierre.noel.dev@gmail.com>
2019-05-27 21:54:27 +0200

    Implement test unit WIP

---
+++ Assets/Rekkuzan/Helper/Editor/TestUnit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using System.Collections;
+
+namespace Rekkuzan.Helper
+{
+    public static class TestUnit
+    {
+        [Test]
+        public static void ClampVector3()
+        {
+            Vector3 Vec = new Vector3(-5, 10, 15);
+            Vector3 ClampVec3 = Vec.Clamp(-1, 1);
+            Debug.Assert(ClampVec3.x == -1 && ClampVec3.y == 1 && ClampVec3.z == 1, "Vector.Clamp not working");
+        }
+
+        [Test]
+        public static void SetActiveGameObject()
+        {
+            GameObject Test = new GameObject();
+            Test.SetActive(true);
+            Debug.Assert(Test.activeSelf == true, "GameObject.SetActive() is not working");
+            Test.SetActive(false);
+            Debug.Assert(Test.activeSelf == false, "GameObject.SetActive() is not working");
+            Test.SetActive(false);
+            Test.SetActive(false);
+            Debug.Assert(Test.activeSelf == false, "GameObject.SetActive() is not working");
+        }
+    }
+}
