9ee9f493658c44a5a8432840a2be98f9656440c8
Pierre Noel <pierre.noel.dev@gmail.com>
2021-03-29 23:34:45 +0200

    Add tag attribute

---
+++ Assets/Rekkuzan/Helper/Attributes/TagAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Rekkuzan.Helper
+{
+    /// <summary>
+    /// Attribute to select a single tag
+    /// </summary>
+    public class TagAttribute : PropertyAttribute
+    {
+    }
+}
---
+++ Assets/Sample/TagSampleBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using Rekkuzan.Helper;
+using UnityEngine;
+
+public class TagSampleBehaviour : MonoBehaviour
+{
+    [SerializeField, Tag] string m_tagInspector = "Untagged";
+
+    private void OnValidate()
+    {
+        this.gameObject.tag = m_tagInspector;
+    }
+}
