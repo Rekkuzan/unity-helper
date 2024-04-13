807b43737f39dae0b3ff63a7bfc2dabc41e4dd9b
Pierre <noel.pierre.26@gmail.com>
2024-04-13 10:27:51 +0200

    Add environment variable

---
+++ Packages/com.rekkuzan.utilities/Runtime/Gameplay/EnvironmentVariableHandler.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rekkuzan.Utilities
+{
+    /// <summary>
+    /// Utility to retrieve variable environment.
+    /// <remarks>
+    /// This is an experimental untested feature that requires functional tests.
+    /// Use with caution.
+    /// </remarks>
+    /// </summary>
+    public class EnvironmentVariableHandler : Singleton<EnvironmentVariableHandler>
+    {
+        private readonly Dictionary<string, string> _variableLookup = new Dictionary<string, string>();
+
+        public static string GetEnvironmentVariable(string variableName)
+        {
+            string value = Instance._variableLookup.GetValueOrDefault(variableName, null);
+
+            if(value != null)
+            {
+                return value;
+            }
+
+            try
+            {
+                value = Environment.GetEnvironmentVariable(variableName);
+                Instance._variableLookup.Add(variableName, value);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error getting environment variable: " + ex.Message);
+            }
+            
+            return value;
+        }
+    }
+}
